dcbe30f1b3203cc51dc5da3c5e865a83c7343bdd
PR-Gateway <PR-Gateway@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-13 16:54:19 +0000

    Change index.php
    git-svn-id: https://plugins.svn.wordpress.org/blog2social/trunk@1025218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
  */
 define('PLUGINVERS', '3.0');
 
-register_activation_hook(__FILE__, 'activate_PRG_Connect');
+register_activation_hook(__FILE__, 'activate_blog2social');
 
-register_deactivation_hook(__FILE__, 'deactivate_PRG_Connect');
+register_deactivation_hook(__FILE__, 'deactivate_blog2social');
 
-add_action('admin_menu', 'PRGConnectMenu');
-add_action('init', 'startSession');
-add_action('init', 'updateDB');
-add_action('admin_init', 'PRGConnect_init');
-add_action('post_submitbox_misc_actions', 'publish_box');
+add_action('admin_menu', 'blog2socialMenu');
+add_action('init', 'b2sstartSession');
+add_action('init', 'b2supdateDB');
+add_action('admin_init', 'blog2social_init');
+add_action('post_submitbox_misc_actions', 'b2s_publish_box');
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function setFilterSent($userId, array $setter) {
+function b2ssetFilterSent($userId, array $setter) {
 
     global $wpdb;
     $filterSent = serialize($setter);
@@ -29,13 +29,28 @@ function setFilterSent($userId, array $setter) {
     $wpdb->query($sqlUpdateFilter);
 }
 
-function getFilterSent($userId) {
+function b2sgetFilterSent($userId) {
     global $wpdb;
     $sql = $wpdb->prepare("SELECT `filterSent` FROM `prg_connect_config` WHERE `author_id` = %d", $userId);
     return unserialize($wpdb->get_var($sql));
 }
 
-function activate_PRG_Connect() {
+function activate_blog2social() {
+    $langWP = get_locale();
+    $lang = 'en';
+    if ($langWP == 'de_DE') {
+        $lang = 'de';
+    }
+
+    $textAll = parse_ini_file('languages/lang.ini', TRUE);
+    $text = $textAll[$lang];
+
+    $prgc = is_plugin_active('pr-gateway-connect/index.php');
+    if ($prgc) {
+        deactivate_plugins(basename(__FILE__));
+        wp_die($text['ERROR_ACTIVE_PRGC'], 'Plugin Activation Error', array('response' => 200, 'back_link' => TRUE));
+    }
+
     global $wpdb;
     $sqlCreateFirst = "CREATE TABLE IF NOT EXISTS `prg_connect_sent` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -46,12 +61,6 @@ function activate_PRG_Connect() {
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
     $wpdb->query($sqlCreateFirst);
-
-    $langWP = get_locale();
-    $lang = 'en';
-    if ($langWP == 'de_DE') {
-        $lang = 'de';
-    }
 
     $sqlCreateSecond = "CREATE TABLE IF NOT EXISTS `prg_connect_config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -73,7 +82,7 @@ function activate_PRG_Connect() {
     $wpdb->query($sqlCreateSecond);
 }
 
-function deactivate_PRG_Connect() {
+function deactivate_blog2social() {
     global $wpdb;
     $sqlDeleteFirst = 'DROP TABLE IF EXISTS `prg_connect_sent`';
     $wpdb->query($sqlDeleteFirst);
@@ -81,7 +90,7 @@ function deactivate_PRG_Connect() {
     $wpdb->query($sqlDeleteSecond);
 }
 
-function PRGConnectMenu() {
+function blog2socialMenu() {
     global $wpdb;
     $currentUserID = get_current_user_id();
     $sql = $wpdb->prepare("SELECT * FROM `prg_connect_config` WHERE `author_id` = %d", $currentUserID);
@@ -113,7 +122,7 @@ function PRGConnectMenu() {
     $pages = array();
     add_menu_page('Blog2Social', 'Blog2Social', 'read', 'blog2social', null, plugins_url('/images/logo16x.png', __FILE__));
     $pages[] = add_submenu_page('blog2social', 'Blog2Social', 'Dashboard', 'read', 'blog2social', 'blog2social');
-    
+
     if ($userExist == NULL) {
 
         require_once 'Helper/getToken.php';
@@ -124,12 +133,12 @@ function PRGConnectMenu() {
             $sqlInsertUserBlogToken = $wpdb->prepare("INSERT INTO `prg_connect_config` (`author_id`, `filterSent`, `UserBlogToken`) VALUES (%d, 'a:1:{s:3:\"prg\";b:0;}', %s);", $currentUserID, $result->token);
             $wpdb->query($sqlInsertUserBlogToken);
         }
-    } elseif(substr($userExist->UserBlogToken,0 ,3) != 'v2_') {
+    } elseif (substr($userExist->UserBlogToken, 0, 3) != 'v2_') {
         require_once 'Helper/getToken.php';
 
         $result = json_decode(GetNewToken::tokenFromPRG($currentUserID, get_option('home')));
-        
-        if($result->error == '0') {
+
+        if ($result->error == '0') {
             $sqlUpdateUserBlogToken = $wpdb->prepare("UPDATE `prg_connect_config` SET `UserBlogToken` = %s WHERE `author_id` = '$currentUserID'", $result->token);
             $wpdb->query($sqlUpdateUserBlogToken);
         }
@@ -138,15 +147,15 @@ function PRGConnectMenu() {
     $textAll = parse_ini_file('languages/lang.ini', TRUE);
     $text = $textAll[!empty($userExist->lang) ? $userExist->lang : 'en'];
 
-    $pages[] = add_submenu_page('blog2social', $text['SN'], $text['SN'], 'read', 'prgconfigsocial', 'prgconfigsocial');
-    $pages[] = add_submenu_page('blog2social', $text['SETTINGS'], $text['SETTINGS'], 'read', 'prgpluginconfig', 'prgpluginconfig');
+    $pages[] = add_submenu_page('blog2social', $text['SN'], $text['SN'], 'read', 'b2sconfigsocial', 'b2sconfigsocial');
+    $pages[] = add_submenu_page('blog2social', $text['SETTINGS'], $text['SETTINGS'], 'read', 'b2spluginconfig', 'b2spluginconfig');
 
     if (isset($_SESSION['prg_id'])) {
-        add_submenu_page('blog2social', 'Logout', 'Logout', 'read', 'prglogout', 'prglogout');
+        add_submenu_page('blog2social', 'Logout', 'Logout', 'read', 'b2slogout', 'b2slogout');
     }
 
     foreach ($pages as $var) {
-        add_action($var, 'addStylesnScripts');
+        add_action($var, 'b2saddStylesnScripts');
     }
 }
 
@@ -156,11 +165,10 @@ if ($_SERVER['PHP_SELF'] == '/wp-admin/post-new.php' || $_SERVER['PHP_SELF'] == 
     wp_enqueue_style('FancyBoxStyle', plugins_url('/css/jquery.fancybox-1_3_4.css', __FILE__));
 }
 
-function addStylesnScripts() {
+function b2saddStylesnScripts() {
     wp_enqueue_style('PRGStyle');
     wp_enqueue_style('FancyBoxStyle');
     wp_enqueue_style('Bootstrap');
-//    wp_enqueue_script('JQuery');
     wp_enqueue_script('FancyBox');
     wp_enqueue_script('PRGJS');
     wp_enqueue_script('PRGValidate');
@@ -171,41 +179,53 @@ function blog2social() {
     require_once 'View/listPosts.php';
 }
 
-function prgconfigsocial() {
+function b2sconfigsocial() {
     require_once 'View/configsocial.php';
 }
 
-function prgpluginconfig() {
+function b2spluginconfig() {
     require_once 'View/configPlugin.php';
 }
 
-function prglogout() {
+function b2slogout() {
     session_destroy();
     echo '<meta http-equiv="refresh" content="0; URL=?page=blog2social">';
 }
 
-function startSession() {
+function b2sstartSession() {
     if (!session_id()) {
         session_start();
     }
 }
 
-function updateDB() {
-
+function b2supdateDB() {
     global $wpdb;
 
     $checkTable = $wpdb->get_results("SHOW COLUMNS FROM prg_connect_config");
+    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    $prgc = is_plugin_active('pr-gateway-connect/index.php');
+
+    $langWP = get_locale();
+    $lang = 'en';
+    if ($langWP == 'de_DE') {
+        $lang = 'de';
+    }
+    
+    if (!$checkTable || $prgc) {
+
+        $textAll = parse_ini_file('languages/lang.ini', TRUE);
+        $text = $textAll[$lang];
+
+        deactivate_plugins('blog2social/' . basename(__FILE__));
+        deactivate_plugins('pr-gateway-connect/index.php');
+        wp_die($text['ERROR_ACTIVE_PRGC'], 'Plugin Activation Error', array('response' => 200, 'back_link' => FALSE));
+    }
 
     foreach ($checkTable as $row) {
         $colsConfig[] = $row->Field;
     }
 
     if (!in_array('lang', $colsConfig)) {
-        $langWP = get_locale();
-        $lang = 'en';
-        if ($langWP == 'de_DE') {
-            $lang = 'de';
-        }
         $sqlUpdateTableLang = $wpdb->prepare("ALTER TABLE `prg_connect_config` ADD `lang` varchar(5) NOT NULL DEFAULT %s", $lang);
         $wpdb->query($sqlUpdateTableLang);
     }
@@ -248,8 +268,7 @@ function updateDB() {
     }
 }
 
-function PRGConnect_init() {
-
+function blog2social_init() {
     wp_register_style('PRGStyle', plugins_url('/css/b2s.1_0.css', __FILE__));
     wp_register_script('FancyBox', plugins_url('/js/jquery.fancybox-1_3_4.pack.js', __FILE__));
     wp_register_style('FancyBoxStyle', plugins_url('/css/jquery.fancybox-1_3_4.css', __FILE__));
@@ -259,7 +278,7 @@ function PRGConnect_init() {
     wp_register_script('Bootstrap_full', plugins_url('/js/bootstrap.min.3_0_3.js', __FILE__));
 }
 
-function publish_box() {
+function b2s_publish_box() {
     global $wpdb;
     $currentUserID = get_current_user_id();
     $sql = $wpdb->prepare("SELECT `lang` FROM `prg_connect_config` WHERE `author_id` = %d", $currentUserID);
